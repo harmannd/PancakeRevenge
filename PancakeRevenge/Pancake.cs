cf0d9e7733102bf944d26b586cf029c4acb4bd64
harmannd <derekhar2468@gmail.com>
2016-10-28 14:47:56 -0500

    created pancake stack

---
+++ PancakeRevenge/Pancake.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.PancakeRevenge
+{
+    class Pancake
+    {
+        private char sideUp;
+
+        public Pancake( char sUp )
+        {
+            sideUp = sUp;
+        }
+
+        private void setSide( char sUp )
+        {
+            sideUp = sUp;
+        }
+
+        private char getSide()
+        {
+            return sideUp;
+        }
+    }
+}
--- PancakeRevenge/PancakeRevenge.cs
+++ PancakeRevenge/PancakeRevenge.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
 
 namespace MyProject.PancakeRevenge
 {
     class PancakeRevenge
     {
+        private static Stack st = new Stack();
+
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("C:\\Users\\Derek\\Documents\\CodeJam\\PancakeRevenge\\B-small-practice");
+            StreamWriter sw = new StreamWriter("C:\\Users\\Derek\\Documents\\CodeJam\\PancakeRevenge\\outSmall.txt");
+            int testCases = 0;
+            String str = null;
 
+            str = sr.ReadLine();
+            int.TryParse(str, out testCases);
+            for ( int i = 0; i < testCases; i++ )
+            {
+                str = sr.ReadLine();
+                stackPancakes(str);
+                flipPancakes();
+            }
+        }
+
+        public static void stackPancakes(String pancakeStack)
+        {
+            Stack temp = new Stack();
+            char ch;
+
+            var pancakes = pancakeStack.ToCharArray();
+            foreach ( char p in pancakes )
+            {
+                temp.Push(p);
+            }
+            while ( temp.Count > 0 )
+            {
+                ch = (char)temp.Pop();
+                st.Push(ch);
+            }
+        }
+
+        public static void flipPancakes()
+        {
+            char sideUp = (char)st.Pop();
+            Pancake pan = new MyProject.PancakeRevenge.Pancake(sideUp);
         }
     }
 }
